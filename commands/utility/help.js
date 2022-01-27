d271d72c8372d1b82f8a94e6fde0265e7b239ffb
SuperiorNepu <59150848+SuperiorNepu@users.noreply.github.com>
2022-01-26 22:52:10 -0500

    Added "deposit all" feature to deposit.js
    check if it work cause I really don't know  buddi or not it's just a quality of life thing. Also planning on creating a help command cause you keep neglecting it

---
+++ commands/utility/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'help',
+    aliases: [],
+    permissions: [],
+    cooldown: 3,
+    description: "Shows how to use each command",
+    execute(client, message, args, Discord, profileData) {
+       //WIP 
+    }
+}
